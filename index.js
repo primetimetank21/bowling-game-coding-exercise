99ad56f853d8b64ae491f0f20b64d4079a925b7e
Earl Tankard, Jr <theflashtank21@gmail.com>
2020-02-09 02:05:05 -0500

    Started to create functions -- for creating a frame (keep track of score) and for computing score; need to implement calculations

--- index.js
+++ index.js
@@ -1,33 +1,63 @@
 // Imports
 const prompt = require('prompt-sync')();
 
-// Variable Declarations
-const scorecard = prompt("Enter your scorecard: ");
-let finalScore = 0;
-let i = 0;
-let slashIndexes = [0];
+function frameMaker(points, throws) {
 
-// Gets indexes of '-'
-while (i !== scorecard.length) {
+    let scoreFrame = {
+        score: points,
+        extraThrows: throws
+    };
 
-    if (scorecard.charAt(i) === '-') {
-        slashIndexes.push(i);        
-        // console.log(`dash at index ${i}`);
-    }
-
-    i++;
+    return scoreFrame;
 }
 
-// Start computing score
-for (i = 0; i < slashIndexes.length; i++) {
-    if(i+1 === slashIndexes.length) {
-        //compute til end of string
-        console.log(`Last index is at: ${slashIndexes[i]}`);
-    }
-    else {
-        //compute until the next index
-        console.log(`Index: ${slashIndexes[i]}`);
 
+function computeScore() {
+
+    // Variable Declarations
+    let tmpScorecard = prompt("Enter your scorecard: ");
+    const scorecard = ' ' + tmpScorecard;
+    let finalScore = 0;
+    let i = 0;
+    let frameArray = [];
+    let slashIndexes = [0];
+
+    // Gets indexes of '-'
+    while (i !== scorecard.length) {
+
+        if (scorecard.charAt(i) === '-') {
+            slashIndexes.push(i);
+            // console.log(`dash at: ${i};  char: ${scorecard.charAt(i)}`);
+        }
+
+        i++;
     }
+
+    // console.log(slashIndexes);
+
+    // Start computing score
+    for (let j = 0; j < slashIndexes.length; j++) {
+        if (slashIndexes[j] === slashIndexes[slashIndexes.length-1]) {
+            //compute til end of string
+            for (let k = slashIndexes[j] + 1; k < scorecard.length; k++) {
+                console.log(`char= ${scorecard.charAt(k)}`);
+
+            }
+
+        }
+        else {
+            //compute until the next index
+            // console.log(`Index: ${slashIndexes[j]}`);
+            for (let k = slashIndexes[j] + 1; k < slashIndexes[j + 1]; k++) {
+                console.log(`char= ${scorecard.charAt(k)}`);
+
+            }
+        }
+    }
+
+    return finalScore;
 }
 
+const bowlingScore = computeScore();
+
+console.log(`bowlingScore: ${bowlingScore}`);
